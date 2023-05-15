19a07cea5c11a62dbae9243fb32d62b8a024f30d
Tivatamber <tivatamber@gmail.com>
2023-05-15 20:33:56 +0800

    Smoke and final scene with bvh

--- src/material.h
+++ src/material.h
@@ -109,4 +109,17 @@ public:
     std::shared_ptr<texture> emit;
 };
 
+class isotropic: public material {
+public:
+    isotropic(color c):albedo(std::make_shared<solid_color>(c)) {}
+    isotropic(std::shared_ptr<texture> a):albedo(a) {}
+    bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override {
+        scattered = ray(rec.p, random_in_unit_sphere(), r_in.time());
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
+        return true;
+    }
+public:
+    std::shared_ptr<texture> albedo;
+};
+
 #endif //RAYTRACING_MATERIAL_H
